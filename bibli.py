b9b7a5de41be6b4eb0d2b3c0384fde632023d434
szainaba <saidchaffi.zainaba@etud.univ-angers.fr>
2024-11-28 20:49:20 +0100

    Modification sur simple_bibli

---
+++ bibli.py
@@ -0,0 +1,32 @@
+import numpy as np
+import os
+import requests
+from datetime import datetime
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
+from urllib.request import urlretrieve
+
+class bibli(simple_bibli):
+    def __init__(self, path):
+        super().__init__(path)
+        self.livres = []
+
+    def ajouter(self, livre):
+        self.livres.append(livre)
+
+    def alimenter(self,url):
+        page = requests.get("https://math.univ-angers.fr/~jaclin/biblio/livres/", verify=False)
+        titre = []
+        if page.status_code == 200:
+            soup = BeautifulSoup(page.content, 'lxml')
+            balises_livres = soup.find_all('tr')
+            for balise_livre in balises_livres:
+               link = balise_livre.a
+               if link is not None:
+                    titre.append(link["href"])
+                    if link["href"].endswith("pdf"):
+                        livre = LivrePDF(url+link["href"],link["href"], "unknown", "unknown", "unknown", datetime(2022, 1, 1))
+                        self.ajouter(livre)
+                    elif link["href"].endswith("epub"):
+                        livre = LivreEPUB(url+link["href"],link["href"], "unknown", "unknown", "unknown", datetime(2022, 1, 1))
+                        self.ajouter(livre)
--- simple_bibli.py
+++ simple_bibli.py
@@ -1,1 +1,30 @@
+class simple_bibli(base_bibli):
+    def __init__(self, path):
+        self.path = path
+        self.livres = []
 
+    def ajouter(self, livre):
+        # Ajoute le livre à la bibliothèque
+        self.livres.append(livre)
+
+    def rapport_livres(self, format, fichier):
+        # Génère un rapport sur les livres de la bibliothèque
+        with open(fichier, 'w') as rapport:
+            rapport.write(f"Liste des livres de la bibliothèque au format :\n")
+            for livre in self.livres:
+                rapport.write(f"Titre: {livre.titre()}, Auteur: {livre.auteur()}, Type: {livre.type()}, Fichier: {livre.ressource}\n")
+
+    def rapport_auteurs(self, format, fichier):
+        # Génère un rapport sur les auteurs des livres de la bibliothèque
+        auteurs = {}
+        with open(fichier, 'w') as rapport:
+            rapport.write(f"Liste des auteurs des livres de la bibliothèque :\n")
+            for livre in self.livres:
+                auteur = livre.auteur()
+                if auteur not in auteurs:
+                    auteurs[auteur] = []
+                auteurs[auteur].append(f"Titre: {livre.titre()}, Type: {livre.type()}, Fichier: {livre.ressource}\n")
+            
+            for auteur, livres in auteurs.items():
+                rapport.write(f"Auteur: {auteur}\n")
+                rapport.write("".join(livres))
