8f6c5e00870b628646fa4329db140a489061c1af
Prosper1325 <147058242+Prosper1325@users.noreply.github.com>
2024-12-02 11:22:08 +0100

    Update CodeTest1.py

--- CodeTest1.py
+++ CodeTest1.py
@@ -1,31 +1,11 @@
+async def main():
+
+    Bibli = bibli("C:/Users/utilisateur/Documents/Biblio")
+    d = BibliScrap("C:/Users/utilisateur/Documents/Biblio")
+    await Bibli.alimenter("https://math.univ-angers.fr/~jaclin/biblio/livres/")
+    await d.scrap("https://math.univ-angers.fr/~jaclin/biblio/livres/", profondeur=1, nbmax=100)
+    Bibli.rapport_livres("PDF", r"C:\Users\utilisateur\Documents\Biblio\rapport_livres.txt")
+    Bibli.rapport_auteurs("PDF", r"C:\Users\utilisateur\Documents\Biblio\rapport_auteurs.txt")
+          
 if __name__ == "__main__":
-    # Création d'une instance de Bibli
-
-    bibli = bibli(r"C:\Users\utilisateur\Biblio")  #### lien pour windows pour recevoir les rapports
-
-    # Création d'instances de LivreEPUB et LivrePDF
-    livre_epub = LivreEPUB(r"C:\Users\utilisateur\Biblio\pdf.txt", "Titre EPUB", "Auteur EPUB", "Français", "Science Fiction", datetime(2022, 1, 1))
-    livre_pdf = LivrePDF(r"C:\Users\utilisateur\Biblio\Ebook.txt", "Titre PDF", "Auteur PDF", "Anglais", "Informatique", datetime(2022, 2, 1))
-
-    # Ajout des livres à la bibliothèque
-    bibli.ajouter(livre_epub)
-    bibli.ajouter(livre_pdf)
-
-    # Génération de rapports
-    bibli.rapport_livres("PDF", r"C:\Users\utilisateur\rapport_livres.txt")
-    bibli.rapport_auteurs("EPUB", r"C:\Users\utilisateur\rapport_auteurs.txt")
-
-    # Alimenter la bibliothèque depuis une URL
-    bibli.alimenter("https://math.univ-angers.fr/~jaclin/biblio/livres/")
-
-    # Vous pouvez maintenant accéder aux livres ajoutés à la bibliothèque
-    for livre in bibli.livres:
-        print(f"Titre: {livre.titre()}, Auteur: {livre.auteur()}, Type: {livre.type()}")
-    Bibli2 = Bibli(r"C:\Users\utilisateur\Biblio")
-    Bibli2.alimenter("https://math.univ-angers.fr/~jaclin/biblio/livres/")
-    Bibli2.rapport_livres("PDF", r"C:\Users\utilisateur\rapport_livres.txt")
-    Bibli2.rapport_auteurs("EPUB", r"C:\Users\utilisateur\rapport_auteurs.txt")
-
-    # Exemple d'utilisation de BibliScrap
-    d = BibliScrap()
-    d.scrap("https://math.univ-angers.fr/~jaclin/biblio/livres/", profondeur=2, nbmax=5)
+    asyncio.run(main())
