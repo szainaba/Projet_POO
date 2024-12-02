104a183ef8a8af61f092d38663188cd34daee30c
Prosper1325 <147058242+Prosper1325@users.noreply.github.com>
2024-12-02 19:16:44 +0100

    Update POO.py

--- POO.py
+++ POO.py
@@ -450,8 +450,8 @@ async def main():
     d = bibli_scrap("C:/Users/utilisateur/Documents/Biblio")
     await Bibli.alimenter("https://math.univ-angers.fr/~jaclin/biblio/livres/")
     await d.scrap("https://math.univ-angers.fr/~jaclin/biblio/livres/", profondeur=1, nbmax=100)
-    Bibli.rapport_livres("PDF", r"C:\Users\utilisateur\Documents\Biblio\rapport_livres.txt")
-    Bibli.rapport_auteurs("PDF", r"C:\Users\utilisateur\Documents\Biblio\rapport_auteurs.txt")
+    Bibli.rapport_livres("PDF", r"C:\Users\utilisateur\Documents\Biblio\rapport_livres.pdf")
+    Bibli.rapport_auteurs("PDF", r"C:\Users\utilisateur\Documents\Biblio\rapport_auteurs.pdf")
           
 if __name__ == "__main__":
     asyncio.run(main())
