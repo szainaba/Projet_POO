974689097835589ccff0f22eff5bf6042dffaf5e
Prosper1325 <147058242+Prosper1325@users.noreply.github.com>
2024-12-02 11:18:02 +0100

    Update bibli.py

--- bibli.py
+++ bibli.py
@@ -5,25 +5,79 @@ from simple_bibli import simple_bibli
 from LivreEPUB import *
 from LivrePDF import *
 class bibli(simple_bibli):
-    def __init__(self, path):
+    def __init__(self,path):
+        """ path désigne le répertoire contenant les livres de cette bibliothèque """
         super().__init__(path)
-        self.livres = []
-    def ajouter(self, livre):
-        self.livres.append(livre)
+        self._path = path
+        self._livres = []
 
-    def alimenter(self,url):
-        page = requests.get("https://math.univ-angers.fr/~jaclin/biblio/livres/", verify=False)
-        titre = []
+    def ajouter(self, livre):
+        self._livres.append(livre)
+
+    async def alimenter(self,url):
+        page = requests.get(url, verify=False)
+
+
         if page.status_code == 200:
             soup = BeautifulSoup(page.content, 'lxml')
-            balises_livres = soup.find_all('tr')
-            for balise_livre in balises_livres:
-               link = balise_livre.a
-               if link is not None:
-                    titre.append(link["href"])
-                    if link["href"].endswith("pdf"):
-                        livre = LivrePDF(url+link["href"],link["href"], "unknown", "unknown", "unknown", datetime(2022, 1, 1))
-                        self.ajouter(livre)
-                    elif link["href"].endswith("epub"):
-                        livre = LivreEPUB(url+link["href"],link["href"], "unknown", "unknown", "unknown", datetime(2022, 1, 1))
-                        self.ajouter(livre)
+
+        # Liste pour stocker les valeurs de href
+        href_list = []
+
+        # Parcourir toutes les balises 'tr'
+        for tr_tag in soup.find_all('tr'):
+            # Trouver la balise 'a' à l'intérieur de chaque balise 'tr'
+            a_tag = tr_tag.find('a')
+            if a_tag and 'href' in a_tag.attrs:
+                # Ajouter la valeur de 'href' à la liste
+                href_list.append(url+a_tag['href'])
+
+        #creer une liste de lien pour les livres en pdf et epub
+        liste_pdf = []
+        liste_epub = []
+
+
+
+        # Parcourir la liste des liens
+        for link in href_list:
+            # Vérifier si le lien se termine par 'pdf'
+            if link.endswith('pdf'):
+                liste_pdf.append(link)
+            # Vérifier si le lien se termine par 'epub'
+            elif link.endswith('epub'):
+                liste_epub.append(link)
+        
+        print("Début de l'alimentation de la bibliothèque")
+        print("\n  Téléchargement des livres en cours...")
+
+        tasks = []
+
+        async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=False)) as session:
+            for url in liste_pdf:
+                file_info = PdfFileInfo()
+                task = asyncio.create_task(file_info.extract_metadata(url))
+                tasks.append(task)
+            # Attendre que toutes les tâches soient terminées
+            results = await asyncio.gather(*tasks)
+            # Créer des objets livre avec les métadonnées extraites
+            for info in results:
+                if info:
+                    livre= PDF_livre(info.ressource, info.title, info.author, info.language, info.subject, info.date)
+                    self.ajouter(livre)
+
+        tasks = []
+        async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=False)) as session:
+            for url in liste_epub:
+                file_info = EpubFileInfo()
+                task = asyncio.create_task(file_info.extract_metadata(url))
+                tasks.append(task)
+            # Attendre que toutes les tâches soient terminées
+            results = await asyncio.gather(*tasks)
+            # Créer des objets livre avec les métadonnées extraites
+            for info in results:
+                if info:
+                    livre= EPUB_livre(info.ressource, info.title, info.author, info.language, info.subject, info.date)
+                    self.ajouter(livre)
+
+            print("Fin de l'alimentation de la bibliothèque")        
+
